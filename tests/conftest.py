4bc7c8e250dc450ecd175a92557ad3aa53d0a703
aliadde <aliadely1384@gmail.com>
2026-07-30 22:59:45 +0330

    [feat] tests configuration created.
    conftest.py and strcuture of the tests/ have been done.

---
+++ tests/conftest.py
@@ -0,0 +1,46 @@
+import pytest, os, dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from app.db.database import get_db
+from fastapi.testclient import TestClient
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# dotenv.load_dotenv()
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_db():
+    Base.metadata.create_all(bind=engine)
+    yield TestSessionLocal()
+    Base.metadata.drop_all(bind=engine) 
+    
+@pytest.fixture(scope="function", autouse=True)
+def db_session(request, setup_db):
+    db = setup_db
+    try:
+        yield db
+    finally:
+        db.close()
+        
+
+@pytest.fixture(scope="function")
+def client(db_session):
+    def override_get_db():
+        yield db_session
+    
+    from app.main import app
+    app.dependency_overrides[get_db] = override_get_db
+    
+    # ==== This is one way :
+    # return app.test_client() 
+    
+    # ====== another way
+    with TestClient(app) as client:
+        yield client
+    app.dependency_overrides.clear()
