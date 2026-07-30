53a2c4d3981eb59252dc7a0eb102600b19671188
aliadde <aliadely1384@gmail.com>
2026-07-30 19:51:25 +0330

    [feat] database connection create.
    in config.py : we have initialized both development database and
    postgresql database (in compose docker)
    compose: i have created a yaml (for now) create a postresql database
    container.
    requirements-dev.txt : updated

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     API_V1_STR: str 
     SECRET_KEY: str 
     ACCESS_TOKEN_EXPIRE_MINUTES: int 
+    DATABASE_URL: str
     model_config = SettingsConfigDict( env_file=".env", case_sensitive=True)
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends , HTTPException
 from app.core.config import settings
-
+from app.db.database import get_db
 
 app = FastAPI(
     title=settings.APP_NAME,
     debug=settings.DEBUG,
 )
 
+# ============= database config  ================
+from app.models.users import Users
+from app.models.companies import Companies
+from app.models.jobs import Jobs
+# from app.models.applications import Applications
+from sqlalchemy.orm import Session
+from app.db.database import  engine, Base
+# create all tables in the database
+Base.metadata.create_all(bind=engine)
+
+# ================================================
+
+
 
 @app.get("/")
-def root():
+def root(db : Session = Depends(get_db)):
+    
     return {
-        "message": "Job Tracker API"
+        "users": db.query(Users).all()
     }
