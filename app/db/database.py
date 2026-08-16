59a080daec5549e6e4530f0f4abedfa17fe622e9
aliadde <aliadely1384@gmail.com>
2026-08-16 18:54:02 +0330

    [lifespan] the creation of tables and seeders change on startup.
    - now we use default built-in lifespan of FastAPI for creating tables
      and seeders.

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from app.db.seeeder import main as init_seeder
 
 import dotenv 
 import os 
@@ -15,16 +14,13 @@ SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine
 
 class Base(DeclarativeBase):
     pass
-    
+
 
 async def get_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
     db = SessionLocal()
 
-    # seeder create jobs and statuses and positions
-    await init_seeder(SessionLocal)
+    
     
     try:
         yield db
--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from app.core.config import settings
 
 
+async def create_tables(engine, Base):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    from app.db.database import SessionLocal, engine, Base
+    from app.db.seeeder import main as init_seeder
+
+    async with engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    async with SessionLocal() as db:
+        # seeder create jobs and statuses and positions
+        await init_seeder(SessionLocal)
+
+    yield
+    
+    
 app = FastAPI(
     title=settings.APP_NAME,
     debug=settings.DEBUG,
+    lifespan=lifespan,
 )
-
 
 # ==== Routers ====
 # Import the auth module AFTER creating the router
@@ -19,5 +40,5 @@ app.include_router(metadata.router, prefix='/metadata', tags=["Metadata"])
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 # ==============================================
